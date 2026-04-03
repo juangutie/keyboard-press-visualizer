cf1c6174072e7326b8f97e5e678cb1949606b6ee
Juan Gutierrez <24966258+juangutie@users.noreply.github.com>
2026-04-02 21:52:42 -0400

    refactor(service-worker.js): for easier versioning

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,40 @@
+const REPOSITORY = "keyboard-press-visualizer";
+const CACHE_VERSION = "v2"
+const URLS = [
+    "/",
+    "/favicon.ico",
+    "/index.html",
+].map((url) => `/${REPOSITORY}${url}`);
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("keyboard-press-visualizer_v1").then((cache) =>
-            cache.addAll([
-                "/keyboard-press-visualizer/",
-                "/keyboard-press-visualizer/favicon.ico",
-                "/keyboard-press-visualizer/index.html",
-            ])
+        caches.open(`${REPOSITORY}_${CACHE_VERSION}`).then((cache) =>
+            cache.addAll(URLS)
         )
     );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) =>
-      response ?? fetch(event.request)
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys.filter((key) =>
+                key.startsWith(REPOSITORY) && !key.endsWith(CACHE_VERSION)
+            ).map((key) =>
+                caches.delete(key)
+            )  
+        ))
     )
-  );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) =>
+            response ?? fetch(event.request).catch((error) => 
+                new Response(error, {
+                    status: 408,
+                    headers: {"Content-Type": "text/plain"}
+                })
+            )
+        )
+    );
 });
